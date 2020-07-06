a225bbfd38601e86a547e840da7a1e080528f886
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-07-06 08:39:00 -0400

    Update Stat.js

--- commands/Info/Stat.js
+++ commands/Info/Stat.js
@@ -8,7 +8,7 @@ module.exports = {
     run: (client, message, args) => {
     const embed = new RichEmbed()
     .setThumbnail("https://cdn.glitch.com/34219f89-5da9-4107-b722-a71edf79699b%2Ftenor.gif?v=1590626769885")
-    .setTitle("<a:NitroBoost728:709614699041259570>   Stats   <a:NitroBoost728:709614699041259570>")
+    .setTitle("<a:rgbsheep:718908127524946012>   Stats   <a:rgbsheep:718908127524946012>")
     .setDescription("-------------")
     .setColor("RANDOM")
     .addField("Command called by", message.author.username)
